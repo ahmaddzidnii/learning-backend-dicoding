944c2ceaf1bcd276a40f76b71c0c1813c73adf7e
ahmaddzidnii <zdngaming312@gmail.com>
2023-10-09 19:56:05 +0700

    hari ke 2

---
+++ Belajar Dasar Pemrograman JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,30 @@
+
+// class Animal {
+//   constructor(name, age, isMammal) {
+//     (this.name = name), (this.age = age), (this.isMammal = isMammal);
+//   }
+// }
+
+// class Rabbit extends Animal {
+//   constructor(name, age) {
+//     super(name, age, true);
+//   }
+//   eat() {
+//     return `${this.name} sedang makan!`;
+//   }
+// }
+
+// class Eagle extends Animal {
+//   constructor(name, age) {
+//     super(name, age, false);
+//   }
+
+//   fly() {
+//     return `${this.name} sedang terbang!`;
+//   }
+// }
+
+// const myRabbit = new Rabbit("Labi", 2);
+// const myEagle = new Eagle("Elo", 4);
+// console.log(myRabbit);
+// console.log(myEagle);
